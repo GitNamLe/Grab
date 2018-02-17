588d530460b0d5c0a4953f2203e5d1b806cd5664
NamLe <vnamle1@gmail.com>
2018-02-16 20:59:05 -0500

    working mobile ocr

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,93 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Camera } from '@ionic-native/camera';
+
+import * as $ from 'jquery'
+import * as jQuery from 'jquery';
+import { ImageService } from '../../services/ocr.service';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  public base64Image: string;
+  cameraErr: string;
+  blob: any;
+  constructor(private camera: Camera, private imageService: ImageService) {
   }
+
+  //not yet until we have url => base64 => blob and then we can test for cordova on ionic view
+  takePicture(){
+    this.camera.getPicture({
+        destinationType: this.camera.DestinationType.DATA_URL,
+        targetWidth: 1000,
+        targetHeight: 1000
+        }).then((imageData) => {
+          // imageData is a base64 encoded string
+          this.base64Image = "data:image/jpeg;base64," + imageData;
+        }, (err) => {
+          this.cameraErr = err;
+        });
+
+/*      let blob = this.imageService.makeblob(this.base64Image); 
+        this.processImage(blob) */
+        this.blob = this.imageService.makeBlob(this.base64Image);
+  };
+
+  processImage() {
+    // **********************************************
+    // *** Update or verify the following values. ***
+    // **********************************************
+
+    // Replace the subscriptionKey string value with your valid subscription key.
+    var subscriptionKey = "6e1e785baae34fd3a857005712ab7810";
+
+    // Replace or verify the region.
+    //
+    // You must use the same region in your REST API call as you used to obtain your subscription keys.
+    // For example, if you obtained your subscription keys from the westus region, replace
+    // "westcentralus" in the URI below with "westus".
+    //
+    // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+    // a free trial subscription key, you should not need to change this region.
+    var uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr";
+
+    // Request parameters.
+    var params = {
+        "language": "unk",
+        "detectOrientation ": "true",
+    };
+
+    // Perform the REST API call.
+    $.ajax({
+        url: uriBase + "?" + $.param(params),
+
+        // Request headers.
+        beforeSend: function(jqXHR){
+            jqXHR.setRequestHeader("Content-Type","application/octet-stream");
+            jqXHR.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+        },
+
+        type: "POST",
+
+        // Request body.
+        data: this.blob,
+        processData: false
+    })
+
+    .done(function(data) {
+        // Show formatted JSON on webpage.
+        $("#responseTextArea").val(JSON.stringify(data, null, 2));
+    })
+
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        // Display error message.
+        var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+        errorString += (jqXHR.responseText === "") ? "" : (jQuery.parseJSON(jqXHR.responseText).message) ? 
+            jQuery.parseJSON(jqXHR.responseText).message : jQuery.parseJSON(jqXHR.responseText).error.message;
+        alert(errorString);
+    });
+};
 
 }
---
+++ src/services/ocr.service.ts
@@ -0,0 +1,37 @@
+
+export class ImageService {
+
+    getBase64Image(img) {
+        var canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+        let ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
+        var dataURL = canvas.toDataURL("image/png");
+        return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+    }
+
+
+    makeBlob(dataURL) {
+        let BASE64_MARKER = ';base64,';
+        if (dataURL.indexOf(BASE64_MARKER) == -1) {
+            let parts = dataURL.split(',');
+            let contentType = parts[0].split(':')[1];
+            let raw = decodeURIComponent(parts[1]);
+            return new Blob([raw], { type: contentType });
+        }
+        let parts = dataURL.split(BASE64_MARKER);
+        let contentType = parts[0].split(':')[1];
+        let raw = window.atob(parts[1]);
+        let rawLength = raw.length;
+
+        let uInt8Array = new Uint8Array(rawLength);
+
+        for (var i = 0; i < rawLength; ++i) {
+            uInt8Array[i] = raw.charCodeAt(i);
+        }
+
+        return new Blob([uInt8Array], { type: contentType });
+    }
+
+}
