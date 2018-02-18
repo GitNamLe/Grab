3ee7c98fa1f46c9bc5e3a0a3fe6674cb4957b759
NamLe <vnamle1@gmail.com>
2018-02-18 08:38:26 -0500

    final

--- src/pages/ocr-render/ocr-render.ts
+++ src/pages/ocr-render/ocr-render.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { ImageService } from '../../services/ocr.service';
 
-import * as MsTranslator from 'mstranslator';
-
 @Component({
   selector: 'page-ocr-render',
   templateUrl: 'ocr-render.html',
@@ -17,11 +15,6 @@ export class OcrRenderPage {
   viewEx1 = false;
   viewEx2 = false;
   viewEx3 = false;
-  // translator api
-  result = "hello wah";
-  client = new MsTranslator({
-      api_key: "ff8a23977cc846d8bfcca73c6b5f06c0"
-  }, true);
 
 
   constructor(public navCtrl: NavController, 
@@ -50,18 +43,6 @@ export class OcrRenderPage {
     }
   }
 
-  getES(){
-    let words =  this.wordsArr.join(" ");
-    let params = {
-        text: '"' + words + '"'
-        , from: 'en'
-        , to: 'es'
-    };
-    this.client.translate(params, (err, data) => {
-        this.result = data;
-    });
-  this.wordsArr = this.result.split(" ");
-}
 
   notify(){
     console.log("toggled: " + this.toggleImg)
